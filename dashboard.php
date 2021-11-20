240a3db378a2f38fd5face9fb19c3fd4c040669c
roseaneesha <roseaneesha02@gmail.com>
2021-11-20 12:06:16 +0530

    dashboard style

--- dashboard.php
+++ dashboard.php
@@ -19,6 +19,9 @@ $results = mysqli_query($db, $query);
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>MyNotes</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Comfortaa:wght@500&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous" />
     <link rel="stylesheet" href="./css/dashboard.css">
@@ -58,13 +61,14 @@ $results = mysqli_query($db, $query);
 
 
     <main>
-
         <div>
-            <h2>Welcome, <?= $_SESSION['session_id']; ?>!
+            <h2 class='welcome text-center p-2'>Welcome, <?= $_SESSION['session_id']; ?>!
 
             </h2>
         </div>
-        <h2>
+
+
+        <h2 class='text-center pt-3 px-3 time'>
 
             <?php
             $dt = date_default_timezone_set("Asia/Kolkata");
@@ -72,70 +76,100 @@ $results = mysqli_query($db, $query);
             ?>
 
         </h2>
-        <h4>
-            <?=
-            date('M j, Y');
+        <div class="date mx-auto">
+            <h4 class='text-center pt-1'>
+                <?=
+                date('M j, Y');
 
-            ?>
-        </h4>
-
-
-        <div class="table-responsive">
-            <table class="table table-hover">
-
-                <thead>
-
-                    <tr>
-                        <th scope="col">Date</th>
-                        <th scope="col">Subject Code</th>
-                        <th scope="col">Subject</th>
-                        <th scope="col">Chapter</th>
-                        <th scope="col">View File</th>
-                        <th scope="col">Delete</th>
-                    </tr>
-
-                </thead>
-
-
-                <tbody>
-                    <tr>
-                        <?php
-
-
-                        while ($row = mysqli_fetch_assoc($results)) :
-                            // var_dump($row); //displays details about the variable
-                        ?>
-
-
-                            <th scope="row"><?= $row["date"] ?></th>
-                            <th scope="row"><?= $row["subCode"] ?></th>
-                            <th scope="row"><?= $row["subject"] ?></th>
-                            <th scope="row"><?= $row["chapter"] ?></th>
-                            <th scope="row">
-                                <a target="_blank" href="./<?= $row["filePath"] ?>">
-                                    <i class="fas icon fa-book-open" style="color:#7047EB;"></i>
-
-                                </a>
-                            </th>
-                            <th scope="row">
-
-                                <i class="fas fa-trash" style="color:#7047EB;"></i> </a>
-
-                            </th>
-
-
-
-
-                    </tr>
-
-
-                <?php endwhile; ?>
-
-
-                </tbody>
-            </table>
-
+                ?>
+            </h4>
         </div>
+
+
+
+        <?php
+        if (mysqli_num_rows($results) == 0) { ?>
+            <div class="table-responsive p-3">
+                <table class="table table-borderless">
+                    <thead>
+
+                        <tr>
+                            <th scope=" col">DATE</th>
+                            <th scope="col">CODE</th>
+                            <th scope="col">SUBJECT</th>
+                            <th scope="col">CHAPTER</th>
+                            <th scope="col">VIEW</th>
+                            <th scope="col">DELETE</th>
+                        </tr>
+
+                    </thead>
+                </table>
+            </div>
+
+
+
+            <h3 class='empty text-center p-3'>Oops, No uploads yet!</h3>
+
+            </div>
+        <?php } else { ?>
+
+            <div class="table-responsive p-3">
+                <table class="table table-hover">
+
+                    <thead>
+
+                        <tr>
+                            <th scope="col">Date</th>
+                            <th scope="col">Code</th>
+                            <th scope="col">Subject</th>
+                            <th scope="col">Chapter</th>
+                            <th scope="col">View</th>
+                            <th scope="col">Delete</th>
+                        </tr>
+
+                    </thead>
+
+
+                    <tbody>
+                        <tr>
+
+                            <?php
+                            while ($row = mysqli_fetch_assoc($results)) :
+
+                                // var_dump($row); //displays details about the variable
+                            ?>
+
+
+                                <th scope="row"><?= $row["date"] ?></th>
+                                <th scope="row"><?= $row["subCode"] ?></th>
+                                <th scope="row"><?= $row["subject"] ?></th>
+                                <th scope="row"><?= $row["chapter"] ?></th>
+                                <th scope="row">
+                                    <a target="_blank" href="./<?= $row["filePath"] ?>">
+                                        <i class="fas icon fa-book-open" style="color:#7047EB;"></i>
+
+                                    </a>
+                                </th>
+                                <th scope="row">
+
+                                    <i class="fas fa-trash" style="color:#7047EB;"></i> </a>
+
+                                </th>
+
+
+
+
+                        </tr>
+
+
+                <?php endwhile;
+                        } ?>
+
+
+                    </tbody>
+                </table>
+
+            </div>
     </main>
 
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
